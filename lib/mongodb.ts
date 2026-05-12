22294fc07f1bf24546b94c1ccc825adfd6886eb2
Artreeus <142108262+Artreeus@users.noreply.github.com>
2026-05-12 23:52:45 +0600

    feat: add MongoDB connection event logging

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -32,7 +32,12 @@ async function dbConnect() {
     };
 
     cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      console.log('Connected to MongoDB');
       return mongoose;
+    });
+
+    mongoose.connection.on('error', (err) => {
+      console.error('MongoDB connection error:', err);
     });
   }
 
